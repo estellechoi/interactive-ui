97b9ed0cb51e227e71ca1ea0cc3acb0e04a1fc5c
Estelle Choi <estele.choi@gmail.com>
2020-11-01 03:17:32 +0900

    Nav component

---
+++ src/components/Navigation.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.div`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+`;
+
+const MenuText = styled.span`
+    padding: 5px 5px;
+    font-weight: 900;
+    transition: color 0.4s;
+
+    &:hover {
+        color: #000;
+    }
+`;
+
+
+export default function Navigation({ menu }) {
+    return (
+        <Nav>
+            {menu.map((item, i) => (
+                <MenuText key={i}>
+                    <Link to={item.path}>{item.name}</Link>
+                </MenuText>
+            ))}
+        </Nav>
+    );
+    
+}
+
+Navigation.defaultProps = {
+    menu: [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "ScrollProgress",
+            path: "/scroll-progress"
+        },
+    ]
+}
