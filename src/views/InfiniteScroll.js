24c2d05fccdacb2671e93d3d8ddc22cb44ea5aa3
Estelle Choi <estele.choi@gmail.com>
2020-11-01 03:50:24 +0900

    Infinite scroll-able view

--- src/views/InfiniteScroll.js
+++ src/views/InfiniteScroll.js
@@ -1,5 +1,38 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+import styled from "styled-components";
+import { useEventListener } from "./../hooks/useEventListener"
+
+const Section = styled.section`
+    background-color: #252526;
+    font-size: 3rem;
+    font-weight: 700;
+    color: #fff;
+`;
 
 export default function InfiniteScroll() {
-    return <></>;
+
+    const [datalist, setDatalist] = useState(new Array(50).fill(0));
+    const section = useRef();
+    
+    const fetchData = () => {
+        setDatalist((prev) => prev.concat(new Array(10).fill(0)));
+    }
+    
+    const checkScrollY = () => {
+        const scrollY = window.scrollY;
+        const maxY = section.current.clientHeight - window.innerHeight;
+
+        if (scrollY >= maxY) fetchData();
+    }
+
+    useEventListener(window, "scroll", checkScrollY);
+
+    return (
+        <Section ref={section}>
+            <h2 className="hidden">Infinite Scroll-able View</h2>
+            <div className="">
+                {datalist.map((item, i) => <div key={i}>{item}</div>)}
+            </div>
+        </Section>
+    );
 }
