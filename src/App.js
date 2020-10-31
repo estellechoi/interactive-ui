7be63bac2deb2039ee2677ba89aa3f6f3a15528d
Estelle Choi <estele.choi@gmail.com>
2020-11-01 03:16:09 +0900

    Routers added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./views/Home";
+import ScrollProgress from "./views/ScrollProgress";
+
+
 export default function App() {
   return (
     <div className="App">
+      <HashRouter>
+        <Navigation/>
+        <Route path="/" exact={true} component={Home}></Route>
+        <Route path="/scroll-progress" component={ScrollProgress}></Route>
+      </HashRouter>
     </div>
   );
 }
