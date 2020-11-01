cae19f6301c13237790c0e74aa9836c052e3d5f6
Estelle Choi <estele.choi@gmail.com>
2020-11-01 23:55:07 +0900

    Video play section

--- src/views/parallax-scroll/FirstSection.js
+++ src/views/parallax-scroll/FirstSection.js
@@ -19,6 +19,36 @@ const Video = styled.video`
     }
 `;
 
+const IntroTextBox = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation-name: fadein;
+    animation-delay: 0.2s;
+    animation-duration: 3s;
+    animation-fill-mode: both;
+
+    @keyframes fadein {
+        0% {
+            opacity: 0;
+        }
+
+        15% {
+            opacity: 1;
+        }
+
+        100% {
+            opacity: 0;
+        }
+    }
+`;
+
 export default function FirstSection() {
 
     const video = useRef();
@@ -36,6 +66,8 @@ export default function FirstSection() {
                     <div className="hidden">Your browser does not support video play.</div>
                 </Video>
                 <figcaption id="video-cap" className="hidden">A background video</figcaption>
+                
+                <IntroTextBox>Hi</IntroTextBox>
             </Fig>
         </>
     );
